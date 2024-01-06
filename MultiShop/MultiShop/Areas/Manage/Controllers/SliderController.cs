58488df560ff59378d3757e32f5c4408dc19b438
Ayshen Salahova <TU81UEBFJ@code.edu.az>
2024-01-07 00:40:55 +0400

    added areas/manage/sliderController

---
+++ MultiShop/MultiShop/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.DAL;
+using MultiShop.Models;
+
+namespace MultiShop.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    
+    public class SliderController : Controller
+    {
+        public AppDbContext _context { get; }
+
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            ICollection<Slider> slider = await _context.Sliders.ToListAsync();
+            return View(slider);
+        }
+    }
+}
